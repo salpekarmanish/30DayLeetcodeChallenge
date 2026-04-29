7c5c0f4275a7a0be7023d924bd5d06f97dab28ca
salpekarmanish <159130652+salpekarmanish@users.noreply.github.com>
2026-04-30 03:19:21 +0530

    Time: 0 ms (100%), Space: 43.9 MB (77.29%) - LeetHub

---
+++ 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+
+        int count = 0;
+
+        for (int h : hours) {
+
+            if (h >= target) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
